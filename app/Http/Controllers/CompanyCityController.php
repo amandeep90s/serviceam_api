e5743a792a4704466907fdc4a5fe99bdc5992fcf
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-24 23:09:57 +0530

    Added City related models

---
+++ app/Http/Controllers/CompanyCityController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCompanyCityRequest;
+use App\Http\Requests\UpdateCompanyCityRequest;
+use App\Models\Common\CompanyCity;
+
+class CompanyCityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompanyCityRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CompanyCity $companyCity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CompanyCity $companyCity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompanyCityRequest $request, CompanyCity $companyCity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CompanyCity $companyCity)
+    {
+        //
+    }
+}
