aee8f25ce7199eb88bead35b6e138403b5a13f7f
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-24 23:05:16 +0530

    Added admin related models

---
+++ app/Http/Controllers/AdminCardController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAdminCardRequest;
+use App\Http\Requests\UpdateAdminCardRequest;
+use App\Models\Common\AdminCard;
+
+class AdminCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdminCardRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AdminCard $adminCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AdminCard $adminCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdminCardRequest $request, AdminCard $adminCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AdminCard $adminCard)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/AdminServiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAdminServiceRequest;
+use App\Http\Requests\UpdateAdminServiceRequest;
+use App\Models\Common\AdminService;
+
+class AdminServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAdminServiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AdminService $adminService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AdminService $adminService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAdminServiceRequest $request, AdminService $adminService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AdminService $adminService)
+    {
+        //
+    }
+}
