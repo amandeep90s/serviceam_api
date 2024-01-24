e1fb2882d91d32586485d62f534c01691b948e56
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-24 22:01:45 +0530

    Added routes files

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Common\CommonController;
+use App\Http\Controllers\Common\Provider\HomeController;
+use App\Http\Controllers\Common\SocialLoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,42 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::prefix('/admin')->group(function () {
+    require_once __DIR__ . '/common/admin.php';
+    require_once __DIR__ . '/order/admin.php';
+    require_once __DIR__ . '/service/admin.php';
+    require_once __DIR__ . '/transport/admin.php';
+});
+
+Route::prefix('/provider')->group(function () {
+    require_once __DIR__ . '/common/provider.php';
+    require_once __DIR__ . '/order/provider.php';
+    require_once __DIR__ . '/service/provider.php';
+    require_once __DIR__ . '/transport/provider.php';
+});
+
+Route::prefix('/user')->group(function () {
+    require_once __DIR__ . '/common/user.php';
+    require_once __DIR__ . '/order/user.php';
+    require_once __DIR__ . '/service/user.php';
+    require_once __DIR__ . '/transport/user.php';
+});
+
+Route::prefix('/order')->group(function () {
+    require_once __DIR__ . '/order/shop.php';
+});
+
+Route::group(['prefix' => '/v1'], function () {
+    Route::post('user/appsettings', [CommonController::class, 'base']);
+    Route::post('provider/appsettings', [CommonController::class, 'base']);
+    Route::get('countries', [CommonController::class, 'countries_list']);
+    Route::get('states/{id}', [CommonController::class, 'states_list']);
+    Route::get('cities/{id}', [CommonController::class, 'cities_list']);
+    Route::post('/chat', [CommonController::class, 'chat']);
+    Route::post('/{provider}/social/login', [SocialLoginController::class, 'handleSocialLogin']);
+    Route::post('/provider/update/location', [HomeController::class, 'update_location']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Common\CommonController;
+use App\Http\Controllers\Common\SocialLoginController;
+use App\Http\Controllers\LicenseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::post('/verify', [LicenseController::class, 'verify'])->name('verify');
+Route::post('/base', [CommonController::class, 'base']);
+Route::post('/search', [CommonController::class, 'search']);
+Route::get('/cmspage/{type}', [CommonController::class, 'cmspagetype']);
+Route::get('/send/{type}/push', [SocialLoginController::class, 'push']);
+
+// Route::get('v1/docs', ['as' => 'swagger-v1-lume.docs', 'middleware' => config('swagger-lume.routes.middleware.docs', []), 'uses' => 'V1\Common\SwaggerController@docs']);
+// Route::get('/api/v1/documentation', ['as' => 'swagger-v1-lume.api', 'middleware' => config('swagger-lume.routes.middleware.api', []), 'uses' => 'V1\Common\SwaggerController@api']);
