a6d0df5a1a76a65889f0a9bc961359d5fb641e58
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-26 15:50:09 +0530

    Added job files

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Helpers\Helper;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -12,12 +13,18 @@ class SendEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $error;
+    protected $company;
+    protected $emails;
+
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($error, $company, $emails)
     {
-        //
+        $this->error = $error;
+        $this->company = $company;
+        $this->emails = $emails;
     }
 
     /**
@@ -25,6 +32,12 @@ class SendEmailJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $subject = '500 Error in ' . $this->company;
+        $templateFile = 'mails/errormail';
+        $data = ['body' => $this->error];
+
+        if (count($this->emails) > 0) {
+            Helper::send_emails_job($templateFile, $this->emails, $subject, $data);
+        }
     }
 }
