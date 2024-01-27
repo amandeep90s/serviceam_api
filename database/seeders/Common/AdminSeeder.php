d6dd079c4099c7824a4de30d0c95d35b6fe7a003
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-27 19:02:36 +0530

    Adding common seed files

--- database/seeders/Common/AdminSeeder.php
+++ database/seeders/Common/AdminSeeder.php
@@ -2,15 +2,113 @@
 
 namespace Database\Seeders\Common;
 
+use App\Models\Common\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
 
 class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run($company = null): void
     {
-        //
+        Schema::disableForeignKeyConstraints();
+
+        $user = Admin::create([
+            "name" => "Admin",
+            "email" => "admin@demo.com",
+            "password" => Hash::make("123456"),
+            "mobile" => "9876543210",
+            "company_id" => $company,
+        ]);
+
+        $role = Role::where("name", "ADMIN")->first();
+
+        if ($role != null) {
+            $user->assignRole($role->id);
+        }
+
+        $current = Admin::create([
+            "name" => "Dispatcher",
+            "email" => "dispatcher@demo.com",
+            "password" => Hash::make("123456"),
+            "mobile" => "9876543211",
+            "company_id" => $company,
+        ]);
+
+        $dispatcher = Role::where("name", "DISPATCHER")->first();
+
+        if ($dispatcher != null) {
+            $current->assignRole($dispatcher->id);
+        }
+
+        $current = Admin::create([
+            "name" => "Dispute",
+            "email" => "dispute@demo.com",
+            "password" => Hash::make("123456"),
+            "mobile" => "9876543212",
+            "company_id" => $company,
+        ]);
+
+        $dispute = Role::where("name", "DISPUTE")->first();
+
+        if ($dispute != null) {
+            $current->assignRole($dispute->id);
+        }
+
+        $user_list = [
+            [
+                "name" => "Daniel",
+                "email" => "brayden@demo.com",
+                "password" => Hash::make("123456"),
+                "mobile" => "9876543213",
+                "company_id" => $company,
+            ],
+            [
+                "name" => "James",
+                "email" => "cina@demo.com",
+                "password" => Hash::make("123456"),
+                "mobile" => "9876543214",
+                "company_id" => $company,
+            ],
+            [
+                "name" => "Emily",
+                "email" => "kain@demo.com",
+                "password" => Hash::make("123456"),
+                "mobile" => "9876543215",
+                "company_id" => $company,
+            ],
+            [
+                "name" => "Jack",
+                "email" => "rigo@demo.com",
+                "password" => Hash::make("123456"),
+                "mobile" => "9876543216",
+                "company_id" => $company,
+            ],
+        ];
+
+        foreach ($user_list as $current_user) {
+            $current = Admin::create($current_user);
+
+            $dispatcher = Role::where("name", "DISPATCHER")->first();
+
+            if ($dispatcher != null) {
+                $current->assignRole($dispatcher->id);
+            }
+        }
+
+        foreach ($user_list as $current_user) {
+            $current = Admin::create($current_user);
+
+            $dispute = Role::where("name", "DISPUTE")->first();
+
+            if ($dispute != null) {
+                $current->assignRole($dispute->id);
+            }
+        }
+        Schema::enableForeignKeyConstraints();
     }
 }
--- database/seeders/Common/AdminServiceSeeder.php
+++ database/seeders/Common/AdminServiceSeeder.php
@@ -3,14 +3,29 @@
 namespace Database\Seeders\Common;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class AdminServiceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run($company = null): void
     {
-        //
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('admin_services')->insert([
+
+            [
+                'admin_service' => 'SERVICE',
+                'display_name' => 'SERVICE',
+                'base_url' => 'http://127.0.0.1:8001/api/v1',
+                'status' => '1',
+                'company_id' => $company
+            ]
+        ]);
+
+        Schema::enableForeignKeyConstraints();
     }
 }
