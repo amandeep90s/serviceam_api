b4658447fb0080c25c0d1eb2eccf95079784eca1
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-27 19:34:21 +0530

    Added seeders in database seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,27 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\Common\AdminSeeder;
+use Database\Seeders\Common\AdminServiceSeeder;
+use Database\Seeders\Common\CitySeeder;
+use Database\Seeders\Common\CmsPageSeeder;
+use Database\Seeders\Common\CommonClearSeeder;
+use Database\Seeders\Common\CompanyCitySeeder;
+use Database\Seeders\Common\CompanySeeder;
+use Database\Seeders\Common\CountrySeeder;
+use Database\Seeders\Common\DemoSeeder;
+use Database\Seeders\Common\DocumentSeeder;
+use Database\Seeders\Common\PermissionSeeder;
+use Database\Seeders\Common\ProviderVehicleSeeder;
+use Database\Seeders\Common\RoleSeeder;
+use Database\Seeders\Common\SettingSeeder;
+use Database\Seeders\Common\StateSeeder;
+use Database\Seeders\Service\DisputeSeeder;
+use Database\Seeders\Service\PromocodeSeeder;
+use Database\Seeders\Service\ReasonSeeder;
+use Database\Seeders\Service\ServiceCategorySeeder;
+use Database\Seeders\Service\ServiceSeeder;
+use Database\Seeders\Service\ServiceSubCategorySeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +33,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CompanySeeder::class,
+            AdminServiceSeeder::class,
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            CountrySeeder::class,
+            StateSeeder::class,
+            CitySeeder::class,
+            SettingSeeder::class,
+            AdminSeeder::class,
+            CompanyCitySeeder::class,
+            DemoSeeder::class,
+            ProviderVehicleSeeder::class,
+            CmsPageSeeder::class,
+            CommonClearSeeder::class,
+            DocumentSeeder::class
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        //SERVICE
+        $this->call([
+            ServiceCategorySeeder::class,
+            ServiceSubCategorySeeder::class,
+            DisputeSeeder::class,
+            ReasonSeeder::class,
+            ServiceSeeder::class,
+            PromocodeSeeder::class
+        ]);
+
     }
 }
