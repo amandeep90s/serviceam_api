aa7ed4c73661703dd6ca518dd3fb878d9bf0f2f9
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-31 14:12:32 +0530

    Optimized traits code

--- app/Traits/Actions.php
+++ app/Traits/Actions.php
@@ -11,87 +11,114 @@ use Illuminate\Support\Facades\Auth;
 
 trait Actions
 {
-
     public mixed $settings;
     public $user;
+    public $company_id;
 
     public function __construct()
     {
+        // Get settings from Helper
         $this->settings = Helper::setting();
 
-        // Check if the user is authenticated before accessing properties
-        $this->user = Auth::guard(strtolower(Helper::getGuard()))->user();
+        // Get the authenticated user
+        $this->user = $this->getAuthenticatedUser();
 
         // Only set $this->company_id if $this->user is not null
         $this->company_id = $this->user ? $this->user->company_id : null;
     }
 
+    // Method to get the authenticated user
+    private function getAuthenticatedUser(): ?Authenticatable
+    {
+        // Check if the user is authenticated before accessing properties
+        return Auth::guard(strtolower(Helper::getGuard()))->user();
+    }
+
+    // Method to remove a model
     public function removeModel($id): JsonResponse
     {
         try {
+            // Find the model by id and delete it
             $model = $this->model->find($id);
             $model->delete();
+            // Return a response with a success message
             return Helper::getResponse(['message' => trans('admin.user_msgs.user_delete')]);
         } catch (\Throwable $e) {
+            // Return a response with an error message if something goes wrong
             return Helper::getResponse(['status' => 404, 'message' => trans('admin.user_msgs.user_not_found'), 'error' => $e->getMessage()]);
         }
     }
 
+    // Method to remove multiple models
     public function removeMultiple(): JsonResponse
     {
-
         try {
+            // Get the ids from the request and explode them into an array
             $request = $this->request;
             $items = explode(',', $request->id);
+            // Destroy the models by ids
             $this->model->destroy($items);
+            // Return a response with a success message
             return Helper::getResponse(['message' => trans('admin.user_msgs.user_delete')]);
         } catch (\Throwable $e) {
+            // Return a response with an error message if something goes wrong
             return Helper::getResponse(['status' => 404, 'message' => trans('admin.user_msgs.user_not_found'), 'error' => $e->getMessage()]);
         }
     }
 
+    // Method to change the status of a model
     public function changeStatus(): JsonResponse
     {
         $request = $this->request;
         try {
+            // Update the status of the model by id
             $this->model->where('id', $request->id)->update(['status' => $request->status]);
+            // Return a response with a success message
             return Helper::getResponse(['message' => trans('admin.user_msgs.user_delete')]);
         } catch (\Throwable $e) {
+            // Return a response with an error message if something goes wrong
             return Helper::getResponse(['status' => 404, 'message' => trans('admin.user_msgs.user_not_found'), 'error' => $e->getMessage()]);
         }
     }
 
+    // Method to change the status of multiple models
     public function changeStatusAll(): JsonResponse
     {
         try {
+            // Get the ids from the request and explode them into an array
             $request = $this->request;
             $items = explode(',', $request->id);
+            // Update the status of the models by ids
             $this->model->whereIn('id', $items)->update(['status' => $request->status]);
+            // Return a response with a success message
             return Helper::getResponse(['message' => trans('admin.user_msgs.user_delete')]);
         } catch (\Throwable $e) {
+            // Return a response with an error message if something goes wrong
             return Helper::getResponse(['status' => 404, 'message' => trans('admin.user_msgs.user_not_found'), 'error' => $e->getMessage()]);
         }
     }
 
-    /**
-     * @throws \Exception
-     */
-    public function sendUserData($mailData): bool
+    // Method to send user data
+    public function sendUserData($mailData)
     {
         try {
+            // Get the settings for the authenticated user's company
             $settings = Setting::where('company_id', Auth::user()->company_id)->first()->settings_data->site;
+            // Check if the settings allow sending emails
             if (!empty($settings->send_email) && $settings->send_email == 1) {
+                // Prepare the email data
                 $toEmail = $mailData['email'] ?? '';
                 $name = $mailData['first_name'] ?? $mailData['name'];
-                //  SEND MAIL TO USER, PROVIDER, FLEET
                 $subject = "Notification";
                 $data = ['body' => $mailData['body'], 'username' => $name, 'contact_mail' => $settings->contact_email, 'contact_number' => $settings->contact_number[0]->number];
                 $templateFile = 'mails/notification_mail';
+                // Send the email
                 Helper::sendEmails($templateFile, $toEmail, $subject, $data);
             }
             return true;
         } catch (\Throwable $e) {
-            throw new Exception($e->getMessage());
+            // Throw an exception if something goes wrong
+            return Helper::getResponse(['status' => 404, 'error' => $e->getMessage()]);
         }
     }
 }
--- app/Traits/Encryptable.php
+++ app/Traits/Encryptable.php
@@ -4,86 +4,61 @@ namespace App\Traits;
 
 trait Encryptable
 {
-    /**
-     * If the attribute is in the encryptable array
-     * then decrypt it.
-     *
-     * @param  $key
-     *
-     * @return false|mixed|string $value
-     */
+    protected string $dbSecret;
+    protected string $encryptionSalt;
+    protected string $encryptionIv;
+
+    public function __construct()
+    {
+        $this->dbSecret = env('DB_SECRET');
+        $this->encryptionSalt = env('ENCRYPTION_SALT');
+        $this->encryptionIv = env('ENCRYPTION_IV');
+    }
+
     public function getAttribute($key): mixed
     {
         $value = parent::getAttribute($key);
         if (in_array($key, $this->encryptable) && $value !== '') {
-            $value = $this->customDecrypt($value, env('DB_SECRET'));
+            $value = $this->customDecrypt($value, $this->dbSecret);
         }
 
         return $value;
     }
 
-    /**
-     * Returns decrypted original string
-     */
     protected function customDecrypt($encrypted_string, $passphrase): bool|string
     {
-
-        $salt = env('ENCRYPTION_SALT');
-        $iv = env('ENCRYPTION_IV');
-        $ciphertext = base64_decode($encrypted_string);
+        $cipherText = base64_decode($encrypted_string);
         $iterations = 999; //same as js encrypting
 
-        $key = hash_pbkdf2("sha512", $passphrase, $salt, $iterations, 64);
+        $key = hash_pbkdf2("sha512", $passphrase, $this->encryptionSalt, $iterations, 64);
 
-        return openssl_decrypt($ciphertext, 'aes-256-cbc', hex2bin($key), OPENSSL_RAW_DATA, $iv);
+        return openssl_decrypt($cipherText, 'aes-256-cbc', hex2bin($key), OPENSSL_RAW_DATA, $this->encryptionIv);
     }
 
-    /**
-     * If the attribute is in the encryptable array
-     * then encrypt it.
-     *
-     * @param $key
-     * @param $value
-     * @return mixed
-     */
     public function setAttribute($key, $value): mixed
     {
         if (in_array($key, $this->encryptable)) {
-            $value = $this->customEncrypt($value, env('DB_SECRET'));
+            $value = $this->customEncrypt($value, $this->dbSecret);
         }
         return parent::setAttribute($key, $value);
     }
 
-    /**
-     * Returns an encrypted & utf8-encoded
-     */
     protected function customEncrypt($pure_string, $passphrase): string
     {
-
-        $salt = env('ENCRYPTION_SALT');
-        $iv = env('ENCRYPTION_IV');
-
         $iterations = 999;
-        $key = hash_pbkdf2("sha512", $passphrase, $salt, $iterations, 64);
+        $key = hash_pbkdf2("sha512", $passphrase, $this->encryptionSalt, $iterations, 64);
 
-        $encrypted_data = openssl_encrypt($pure_string, 'aes-256-cbc', hex2bin($key), OPENSSL_RAW_DATA, $iv);
-
+        $encrypted_data = openssl_encrypt($pure_string, 'aes-256-cbc', hex2bin($key), OPENSSL_RAW_DATA, $this->encryptionIv);
 
         return base64_encode($encrypted_data);
     }
 
-    /**
-     * When need to make sure that we iterate through
-     * all the keys.
-     *
-     * @return array
-     */
     public function attributesToArray(): array
     {
         $attributes = parent::attributesToArray();
         foreach ($this->encryptable as $key) {
             if (isset($attributes[$key])) {
-                $attributes[$key] = $this->customDecrypt($attributes[$key], env('DB_SECRET'));
+                $attributes[$key] = $this->customDecrypt($attributes[$key], $this->dbSecret);
             }
         }
         return $attributes;
